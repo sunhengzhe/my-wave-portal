8cd54f6fe80aa716db770e92e01c8359fdef2ba4
sunhz <rick.sun@yyinedu.com>
2022-05-26 19:16:36 +0800

    MOD: send ETH to waver

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -15,7 +15,7 @@ contract WavePortal {
 
   Wave[] waves;
 
-  constructor() {
+  constructor() payable {
     console.log("Yo yo, I am a contract and I am smart");
   }
 
@@ -26,6 +26,14 @@ contract WavePortal {
     waves.push(Wave(msg.sender, _message, block.timestamp));
 
     emit NewWave(msg.sender, block.timestamp, _message);
+
+    uint256 prizeAmount = 0.0001 ether;
+    require(
+        prizeAmount <= address(this).balance,
+        "Trying to withdraw more money than the contract has."
+    );
+    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+    require(success, "Failed to withdraw money from contract.");
   }
 
   function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,24 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
+
+  /*
+   * Get Contract balance
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
@@ -18,6 +32,15 @@ const main = async () => {
 
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
+
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 };
 
 const runMain = async () => {
