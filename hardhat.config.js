b41e2309095a860bf09f599065d87d0903c5027e
sunhz <rick.sun@yyinedu.com>
2022-05-29 13:48:09 +0800

    MOD: my-epic-nft

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,7 +18,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.1",
   networks: {
     rinkeby: {
       // This value will be replaced on runtime
